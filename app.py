8a345751e00134f257ecb9fc91a8673c4e6af742
udhay24 <udhay2498@gmail.com>
2020-04-28 21:18:57 +0530

    updated requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from datetime import datetime
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +15,22 @@ def homepage():
     <p>It is currently {time}.</p>
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+
+@app.route('/api/v1/data/timeline', methods=['GET'])
+def return_cases_timeline():
+    raw_df = pd.read_csv('static/data/complete.csv', index_col=[0], parse_dates=True)
+
+    coronavirus_df = raw_df.drop(labels=['confirmed1', 'confirmed2', 'Latitude', 'Longitude'], axis=1)
+    states = coronavirus_df['state'].unique()
+    grouped_by_state = coronavirus_df.groupby('state')
+    state_data = [
+        {
+            "state": state,
+            "data": json.loads(grouped_by_state.get_group(state).drop('state', axis=1).to_json(orient='index'))}
+        for state in states
+    ]
+    return json.dumps(state_data)
 
 
 if __name__ == '__main__':
